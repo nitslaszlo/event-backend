760d2731b02a31175d0e394ffe507d2c4c6cc675
nitslaszlo <nitslaszlo@gmail.com>
2023-03-27 09:44:09 +0200

    upgrade

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -1,18 +1,18 @@
 import { NextFunction, Request, Response, Router } from "express";
-// import ISession from "interfaces/session.interface";
-import { Types } from "mongoose";
 
+// import ISession from "interfaces/session.interface";
+// import { Types } from "mongoose";
 import HttpException from "../exceptions/HttpException";
-import IdNotValidException from "../exceptions/IdNotValidException";
-import UserNotFoundException from "../exceptions/UserNotFoundException";
+// import IdNotValidException from "../exceptions/IdNotValidException";
+// import UserNotFoundException from "../exceptions/UserNotFoundException";
 import IController from "../interfaces/controller.interface";
 // import IRequestWithUser from "../interfaces/requestWithUser.interface";
 import authMiddleware from "../middleware/auth.middleware";
-import roleCheckMiddleware from "../middleware/roleCheckMiddleware";
-import validationMiddleware from "../middleware/validation.middleware";
+// import roleCheckMiddleware from "../middleware/roleCheckMiddleware";
+// import validationMiddleware from "../middleware/validation.middleware";
 // import postModel from "../post/post.model";
-import CreateLocationDto from "./location.dto";
-import ILocation from "./location.interface";
+// import CreateLocationDto from "./location.dto";
+// import ILocation from "./location.interface";
 import locationModel from "./location.model";
 
 export default class LocationController implements IController {
@@ -28,84 +28,85 @@ export default class LocationController implements IController {
     private initializeRoutes() {
         // this.router.get(`${this.path}/posts/:id`, authMiddleware, this.getAllPostsOfUserByID);
         // this.router.get(`${this.path}/posts/`, authMiddleware, this.getAllPostsOfLoggedUser);
-        this.router.get(`${this.path}/:id`, authMiddleware, this.getUserById);
+        // this.router.get(`${this.path}/:id`, authMiddleware, this.getUserById);
         this.router.get(this.path, authMiddleware, this.getAllLocations);
 
-        this.router.patch(`${this.path}/:id`, [authMiddleware, validationMiddleware(CreateUserDto, true), roleCheckMiddleware(["admin"])], this.modifyUser);
+        // this.router.patch(`${this.path}/:id`, [authMiddleware,
+        // validationMiddleware(CreateLocationDto, true), roleCheckMiddleware(["admin"])], this.modifyUser);
 
-        this.router.delete(`${this.path}/:id`, [authMiddleware, roleCheckMiddleware(["admin"])], this.deleteUser);
+        // this.router.delete(`${this.path}/:id`, [authMiddleware, roleCheckMiddleware(["admin"])], this.deleteUser);
     }
 
-    private getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+    private getAllLocations = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            this.user.find().then(users => {
-                res.send(users);
+            this.location.find().then(locations => {
+                res.send(locations);
             });
         } catch (error) {
             next(new HttpException(400, error.message));
         }
     };
 
-    private getUserById = async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const id = req.params.id;
-            if (Types.ObjectId.isValid(id)) {
-                // const userQuery = this.user.findById(id);
-                // if (request.query.withPosts === "true") {
-                //     userQuery.populate("posts").exec();
-                // }
+    // private getUserById = async (req: Request, res: Response, next: NextFunction) => {
+    //     try {
+    //         const id = req.params.id;
+    //         if (Types.ObjectId.isValid(id)) {
+    //             // const userQuery = this.user.findById(id);
+    //             // if (request.query.withPosts === "true") {
+    //             //     userQuery.populate("posts").exec();
+    //             // }
 
-                // Multiple populates:
-                const user = await this.user.findById(id);
-                if (user) {
-                    res.send(user);
-                } else {
-                    next(new UserNotFoundException(id));
-                }
-            } else {
-                next(new IdNotValidException(id));
-            }
-        } catch (error) {
-            next(new HttpException(400, error.message));
-        }
-    };
+    //             // Multiple populates:
+    //             const user = await this.user.findById(id);
+    //             if (user) {
+    //                 res.send(user);
+    //             } else {
+    //                 next(new UserNotFoundException(id));
+    //             }
+    //         } else {
+    //             next(new IdNotValidException(id));
+    //         }
+    //     } catch (error) {
+    //         next(new HttpException(400, error.message));
+    //     }
+    // };
 
-    private modifyUser = async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const id = req.params.id;
-            if (Types.ObjectId.isValid(id)) {
-                const userData: IUser = req.body;
-                const user = await this.user.findByIdAndUpdate(id, userData, { new: true });
-                if (user) {
-                    res.send(user);
-                } else {
-                    next(new UserNotFoundException(id));
-                }
-            } else {
-                next(new IdNotValidException(id));
-            }
-        } catch (error) {
-            next(new HttpException(400, error.message));
-        }
-    };
+    // private modifyUser = async (req: Request, res: Response, next: NextFunction) => {
+    //     try {
+    //         const id = req.params.id;
+    //         if (Types.ObjectId.isValid(id)) {
+    //             const userData: IUser = req.body;
+    //             const user = await this.user.findByIdAndUpdate(id, userData, { new: true });
+    //             if (user) {
+    //                 res.send(user);
+    //             } else {
+    //                 next(new UserNotFoundException(id));
+    //             }
+    //         } else {
+    //             next(new IdNotValidException(id));
+    //         }
+    //     } catch (error) {
+    //         next(new HttpException(400, error.message));
+    //     }
+    // };
 
-    private deleteUser = async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const id = req.params.id;
-            if (Types.ObjectId.isValid(id)) {
-                const successResponse = await this.user.findByIdAndDelete(id);
-                if (successResponse) {
-                    res.sendStatus(200);
-                } else {
-                    next(new UserNotFoundException(id));
-                }
-            } else {
-                next(new IdNotValidException(id));
-            }
-        } catch (error) {
-            next(new HttpException(400, error.message));
-        }
-    };
+    // private deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+    //     try {
+    //         const id = req.params.id;
+    //         if (Types.ObjectId.isValid(id)) {
+    //             const successResponse = await this.user.findByIdAndDelete(id);
+    //             if (successResponse) {
+    //                 res.sendStatus(200);
+    //             } else {
+    //                 next(new UserNotFoundException(id));
+    //             }
+    //         } else {
+    //             next(new IdNotValidException(id));
+    //         }
+    //     } catch (error) {
+    //         next(new HttpException(400, error.message));
+    //     }
+    // };
 
     // private getAllPostsOfLoggedUser = async (req: IRequestWithUser, res: Response, next: NextFunction) => {
     //     try {
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import App from "./app";
 import AuthenticationController from "./authentication/authentication.controller";
+import LocationController from "./location/location.controller";
 // import PostController from "./post/post.controller";
 // import RecipeController from "./recipe/recipe.controller";
 // import ReportController from "./report/report.controller";
@@ -7,4 +8,4 @@ import UserController from "./user/user.controller";
 
 // new App([new AuthenticationController(), new UserController(), new PostController(), new RecipeController(), new ReportController()]);
 
-new App([new AuthenticationController(), new UserController()]);
+new App([new AuthenticationController(), new UserController(), new LocationController()]);
